6f63760938805400b88191d6706ee6fa95423b4b
Mayko Barbosa <barbosa.mayko@gmail.com>
2025-02-07 18:54:16 -0300

    Update timeController.ts

--- src/controllers/projects/timeController.ts
+++ src/controllers/projects/timeController.ts
@@ -25,62 +25,62 @@ export class TimeController {
             let totalHours = 0;
 
             // Buscar dados de presenças de usuários
-            const result = await prisma.userAttendance.findMany({
-                where: {
-                    AND: [
-                        search ? {
-                            user: {
-                                name: {
-                                    contains: String(search), // Se search for fornecido, filtra pelos nomes dos usuários
-                                }
-                            },
-                        } : {},
-                        {
-                            date: {
-                                gte: new Date(String(start_date)), // Converter para formato Date
-                                lte: new Date(String(deadline)),
-                            },
-                        },
-                        {
-                            company_id: { equals: String(id) },
-                        },
-                    ],
-                },
-                include: {
-                    UserServiceProject: {
-                        select: {
-                            service_project: {
-                                select: {
-                                    price: true,
-                                    Project: {
-                                        select: {
-                                            client: {
-                                                select: {
-                                                    name: true
-                                                }
-                                            }
-                                        }
-                                    }
-                                },
-                            },
-                        },
-                    },
-                    user: {
-                        select: {
-                            id: true,
-                            name: true,
-                            avatar: true,
-                            hourly_price: true,
-                        },
-                    },
+            // const result = await prisma.userAttendance.findMany({
+            //     where: {
+            //         AND: [
+            //             search ? {
+            //                 user: {
+            //                     name: {
+            //                         contains: String(search), // Se search for fornecido, filtra pelos nomes dos usuários
+            //                     }
+            //                 },
+            //             } : {},
+            //             {
+            //                 date: {
+            //                     gte: new Date(String(start_date)), // Converter para formato Date
+            //                     lte: new Date(String(deadline)),
+            //                 },
+            //             },
+            //             {
+            //                 company_id: { equals: String(id) },
+            //             },
+            //         ],
+            //     },
+            //     include: {
+            //         UserServiceProject: {
+            //             select: {
+            //                 service_project: {
+            //                     select: {
+            //                         price: true,
+            //                         Project: {
+            //                             select: {
+            //                                 client: {
+            //                                     select: {
+            //                                         name: true
+            //                                     }
+            //                                 }
+            //                             }
+            //                         }
+            //                     },
+            //                 },
+            //             },
+            //         },
+            //         user: {
+            //             select: {
+            //                 id: true,
+            //                 name: true,
+            //                 avatar: true,
+            //                 hourly_price: true,
+            //             },
+            //         },
 
-                },
-                orderBy: {
-                    check_in_time: "desc"
-                },
-                skip: Number(page) * 10,
-                take: 10,
-            });
+            //     },
+            //     orderBy: {
+            //         check_in_time: "desc"
+            //     },
+            //     skip: Number(page) * 10,
+            //     take: 10,
+            // });
             const resultCount = await prisma.userAttendance.count({
                 where: {
                     AND: [
@@ -103,33 +103,7 @@ export class TimeController {
                     ],
                 },
             })
-            // Formatar e calcular horas trabalhadas
-            const formattedResult = result.map((attendance) => {
-                let hoursWorked = 0;
-
-                if (attendance.check_out_time && attendance.check_in_time) {
-                    hoursWorked = dayjs(attendance.check_out_time).diff(
-                        dayjs(attendance.check_in_time),
-                        "hour",
-                        true
-                    );
-                }
-
-                const roundedHours = parseFloat(hoursWorked.toFixed(2));
-                const calculatedPrice = attendance.user.hourly_price
-                    ? attendance.user.hourly_price * roundedHours
-                    : 0;
-
-                totalPrice += calculatedPrice;
-                totalHours += roundedHours;
-
-                return {
-                    ...attendance,
-                    hours_worked: roundedHours,
-                    price: calculatedPrice,
-                };
-            });
-
+           
             // Contar serviços dentro do período
             const serviceCount = await prisma.serviceProject.count({
                 where: {
@@ -173,7 +147,7 @@ export class TimeController {
                     AND: [
                         {
                             status_project: {
-                                in: ["Pre-Start", "In Progress", "Final walkthrough", "Finished"],
+                                in: ["Pre-Start", "In Progress", "Final walkthrough"],
                             },
                         },
 
@@ -241,6 +215,62 @@ export class TimeController {
                     }
                 }
             });
+
+            // Formatar e calcular horas trabalhadas
+            const formattedResult = projects.map(i => i.serviceProject
+                    .filter(s => s.UserServiceProject.length > 0) // Filtra para garantir que há dados em UserServiceProject
+                    .flatMap(s => s.UserServiceProject
+                        .filter(user => user.user_attendances.length > 0) // Filtra para garantir que há dados em user_attendances
+                        .flatMap(user => user.user_attendances
+                            .map(x => {
+                                let hoursWorked = 0;
+                                if (x.check_out_time && x.check_in_time) {
+                                    hoursWorked = dayjs(x.check_out_time).diff(
+                                        dayjs(x.check_in_time),
+                                        "hour",
+                                        true
+                                    );
+                                }
+
+                                const roundedHours = parseFloat(hoursWorked.toFixed(2));
+                                const calculatedPrice = x.user.hourly_price
+                                    ? x.user.hourly_price * roundedHours
+                                    : 0;
+                                return ({
+                                    ...x,
+                                    hours_worked: roundedHours,
+                                    price: calculatedPrice
+                                })
+                            })
+                        )
+                    )
+            );            
+                // result.map((attendance) => {
+                // let hoursWorked = 0;
+
+                // if (attendance.check_out_time && attendance.check_in_time) {
+                //     hoursWorked = dayjs(attendance.check_out_time).diff(
+                //         dayjs(attendance.check_in_time),
+                //         "hour",
+                //         true
+                //     );
+                // }
+
+                // const roundedHours = parseFloat(hoursWorked.toFixed(2));
+                // const calculatedPrice = attendance.user.hourly_price
+                //     ? attendance.user.hourly_price * roundedHours
+                //     : 0;
+
+                // totalPrice += calculatedPrice;
+                // totalHours += roundedHours;
+
+            //     return {
+            //         ...attendance,
+            //         hours_worked: roundedHours,
+            //         price: calculatedPrice,
+            //     };
+            // });
+
             const projectFormatted = projects.map(i => ({
                 clientData: i.client?.name + ' - ' + i.client?.location,
                 serviceCount: i.serviceProject.length,
