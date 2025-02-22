82302663555d03918d97eaea3a59146025060ab9
Mayko Barbosa <barbosa.mayko@gmail.com>
2025-02-22 12:42:55 -0300

    Update FindWorkedHoursProjectController.ts

--- src/controllers/WorkedHours/FindWorkedHoursProjectController.ts
+++ src/controllers/WorkedHours/FindWorkedHoursProjectController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
+import dayjs from "dayjs";
 
 export class FindWorkedHoursProjectController {
     async handle(request: Request, response: Response) {
@@ -15,28 +16,21 @@ export class FindWorkedHoursProjectController {
             if (name_user) {
                 filtro.name_user = { contains: name_user };
             }
-
+            const dateStart = new Date(date_initial);
+            dateStart.setUTCHours(0, 0, 0, 0);
+            const dateEnd = new Date(date_final);
+            dateEnd.setUTCHours(23, 59, 59, 999);
             if (date_initial && date_final) {
-                const dateStart = new Date(date_initial);
-                dateStart.setUTCHours(0, 0, 0, 0);
-                const dateEnd = new Date(date_final);
-                dateEnd.setUTCHours(23, 59, 59, 999);
                 filtro.start_date = {
                     gte: dateStart.toISOString(),
                     lte: dateEnd.toISOString(),
                 };
             } else if (date_initial) {
-                const dateStart = new Date(date_initial);
-                dateStart.setUTCHours(0, 0, 0, 0);
-                const dateEnd = new Date(date_initial);
-                dateEnd.setUTCHours(23, 59, 59, 999);
                 filtro.start_date = {
                     gte: dateStart.toISOString(),
                     lte: dateEnd.toISOString(),
                 };
             } else if (date_final) {
-                const dateEnd = new Date(date_final);
-                dateEnd.setUTCHours(23, 59, 59, 999);
                 filtro.start_date = {
                     lte: dateEnd.toISOString(),
                 };
@@ -63,17 +57,79 @@ export class FindWorkedHoursProjectController {
                 },
             });
 
+
+            const resultAttendance = await prisma.userAttendance.findMany({
+                where: {
+                    AND: [
+                        {
+                            UserServiceProject: {
+                                service_project: {
+                                    projectId: {
+                                        equals: project_id
+                                    }
+                                }
+                            },
+                        },
+                        
+                    ]
+                   
+                    
+                },
+                include: {
+                    UserServiceProject: {
+                        select: {
+                            service_project: {
+                                select: {
+                                    price: true,
+                                }
+                            }
+                        }
+                    },
+                    user: {
+                        select: {
+                            id: true,
+                            name: true,
+                            avatar: true,
+                            hourly_price: true
+                        }
+                    }
+                },
+                orderBy: {
+                    check_in_time: "desc"
+                }
+            });
+
+            // Calcular as horas trabalhadas
+            const formattedResult = resultAttendance.map((attendance) => {
+                let hoursWorked = 0;
+                if (attendance.check_out_time) {
+                    hoursWorked = dayjs(attendance.check_out_time).diff(dayjs(attendance.check_in_time), 'hour', true);
+                }
+                return {
+                    id: '',
+                    project_id: '',
+                    name_user: attendance.user.name,
+                    amount_of_hours: String(attendance.user.hourly_price),
+                    hourly_price: String(Number(attendance.user.hourly_price) * parseFloat(hoursWorked.toFixed(2))),
+                    data_creation: attendance.check_in_time,
+                    start_date: attendance.check_in_time,
+                    end_date: attendance.check_out_time
+                };
+            });
+
             const total = await prisma.workedhours.count({
                 where: filtro
             });
 
-            return response.json({ total, result });
+            // Combinar os resultados de workedhours e userAttendance
+            const combinedResults = [...result, ...formattedResult];
+            return response.json({ total, result: combinedResults });
         } catch (error) {
             console.error(error);
             if (error instanceof Error) {
                 return response.json({ error: error.message });
             }
-            return response.json({ error: "Internal server error"});
+            return response.json({ error: "Internal server error" });
         }
     }
 }
