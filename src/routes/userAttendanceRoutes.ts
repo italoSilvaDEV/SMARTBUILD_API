68bde85d757340d6896fe3ba52da59f055757d1b
Mayko Barbosa <barbosa.mayko@gmail.com>
2025-03-05 18:53:10 -0300

    [feat] time line

--- src/routes/userAttendanceRoutes.ts
+++ src/routes/userAttendanceRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { UserAttendanceController } from '../controllers/User/UserAttendanceController';
 import { checkToken } from '../middlewares/checkToken';
+import { TimeLineController } from '../controllers/User/TimeLineController';
 const userAttendanceControlller = new UserAttendanceController()
+const timeLineController = new TimeLineController()
 const userAttendanceRoutes = Router();
 
 userAttendanceRoutes.post('/check-in', checkToken, userAttendanceControlller.checkIn);
@@ -22,5 +24,8 @@ userAttendanceRoutes.get(
 );
 
 userAttendanceRoutes.put('/user-attendance/:id/update-times', userAttendanceControlller.updateAttendanceTimes);
+
+userAttendanceRoutes.post('/time-line/check-in', checkToken, timeLineController.handleTimeLine);
+
 
 export { userAttendanceRoutes };
