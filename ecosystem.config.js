6394084019b2386aec3450caf86eeb28829f06e0
Mayko Barbosa <barbosa.mayko@gmail.com>
2025-03-13 17:54:50 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: "api-smartbuild",
+        script: "npm",
+        args: "start",
+        port: 4003,
+        watch: true
+      }
+    ]
+  };
+  
