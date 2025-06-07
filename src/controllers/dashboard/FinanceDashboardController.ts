6109b2ae4205dca777924aeef45b33312487900d
Mayko Barbosa <barbosa.mayko@gmail.com>
2025-06-07 13:53:27 -0400

    [refactor] overTime in cashFlow

--- src/controllers/dashboard/FinanceDashboardController.ts
+++ src/controllers/dashboard/FinanceDashboardController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { prisma } from '../../utils/prisma';
 import { returnPayLoad } from '../../config/returnPayLoad';
 import dayjs from 'dayjs';
+import { calcularHorasTrabalhadas, convertHHMMToDecimal } from '../../utils/calculaHoraExtra';
 
 
 async function validCompany(request: Request) {
@@ -124,16 +125,34 @@ export class FinanceDashboardController {
                 const attendanceCosts = project.serviceProject.flatMap(sp =>
                     sp.UserServiceProject.flatMap(usp =>
                         usp.user_attendances.map(attendance => {
-                            const hoursWorked = attendance.check_in_time && attendance.check_out_time
-                                ? dayjs(attendance.check_out_time).diff(
-                                    dayjs(attendance.check_in_time),
-                                    "hour",
-                                    true
-                                )
-                                : 0;
+                            // const hoursWorked = attendance.check_in_time && attendance.check_out_time
+                            //     ? dayjs(attendance.check_out_time).diff(
+                            //         dayjs(attendance.check_in_time),
+                            //         "hour",
+                            //         true
+                            //     )
+                            //     : 0;
 
+                            let regularHours = 0;
+                            let overtimeHours = 0;
+
+                            if (attendance.check_out_time && attendance.check_in_time) {
+                                const hours = calcularHorasTrabalhadas(
+                                    attendance.check_in_time.toISOString(),
+                                    attendance.check_out_time.toISOString(),
+                                    attendance.workStartTime,
+                                    attendance.workEndTime,
+                                );
+                                regularHours = convertHHMMToDecimal(hours.normais);
+                                overtimeHours = convertHHMMToDecimal(hours.extras);
+                            }
+
+                            const calculatedPrice = attendance.user.hourly_price
+                                ? (regularHours * attendance.user.hourly_price) + (overtimeHours * attendance.user.hourly_price * 1.5)
+                                : 0;
+                            
                             return {
-                                cost: hoursWorked * Number(attendance.user.hourly_price || 0),
+                                cost: calculatedPrice,
                                 date: attendance.check_in_time
                             };
                         })
