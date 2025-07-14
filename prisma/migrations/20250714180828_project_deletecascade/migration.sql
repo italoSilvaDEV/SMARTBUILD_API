fbf1b0086c4ee717d9b637f2964aec1315ec738b
riangon <rian.goncallves@gmail.com>
2025-07-14 15:48:06 -0300

    [fix] migration

--- prisma/migrations/20250714180828_project_deletecascade/migration.sql
+++ prisma/migrations/20250714180828_project_deletecascade/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE `Serviceproject` DROP FOREIGN KEY `ServiceProject_projectId_fkey`;
+ALTER TABLE `ServiceProject` DROP FOREIGN KEY `ServiceProject_projectId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `ServiceProject` ADD CONSTRAINT `ServiceProject_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `project`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
