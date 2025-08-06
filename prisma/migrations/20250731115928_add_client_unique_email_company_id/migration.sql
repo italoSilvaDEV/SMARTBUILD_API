0228e709fcde9042fb8324e05169fc4bdc559931
yurybena <82118965+yurybena@users.noreply.github.com>
2025-08-06 10:38:52 -0300

    [feat]: tirando a obrigatoriedade de unique preparação para merge em produção

--- prisma/migrations/20250731115928_add_client_unique_email_company_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email,company_id]` on the table `Client` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Client_email_company_id_key` ON `Client`(`email`, `company_id`);
