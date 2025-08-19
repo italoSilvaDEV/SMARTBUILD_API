a9e7eab69f47d7fcd8dd7175ce7351d36b37ce25
riangon <rian.goncallves@gmail.com>
2025-08-18 21:00:36 -0300

    [feat] funcionalidade crud estimate

--- src/routes/estimateRoutes.ts
+++ src/routes/estimateRoutes.ts
@@ -4,15 +4,26 @@ import { EstimateController } from "../controllers/projects/EstimateController";
 import multer from "multer";
 import uploadConfig from "../config/uploadUtf8";
 import { GetAllEstimatesByCompanyController } from "../controllers/estimates/getAllEstimatesByCompanyController";
+import { ConvertToProjectController } from "../controllers/estimates/convertToProjectController";
+import { DeleteEstimateController } from "../controllers/estimates/deleteEstimateController";
+import { DeleteServiceEstimateController } from "../controllers/estimates/deleteServiceEstimateController";
 
 const estimateRoutes = Router();
 const estimateController = new EstimateController();
 const getAllEstimatesByCompanyController = new GetAllEstimatesByCompanyController();
+const convertToProjectController = new ConvertToProjectController();
+const deleteEstimateController = new DeleteEstimateController();
+const deleteServiceEstimateController = new DeleteServiceEstimateController();
+
 
 // Configurar multer para aceitar múltiplos arquivos de anexo
 const uploadAttachments = multer(uploadConfig.uploadUtf8("./public/tmp/estimate-attachments"));
 
 estimateRoutes.get("/allestimates/:companyId", checkToken, getAllEstimatesByCompanyController.handle);
+estimateRoutes.post("/convert-to-project", checkToken, convertToProjectController.handle);
+estimateRoutes.delete("/:estimateId", checkToken, deleteEstimateController.handle);
+estimateRoutes.delete("/:id/service/:serviceId/:estimateId", checkToken, deleteServiceEstimateController.handle);
+
 
 estimateRoutes.post("/", checkToken, estimateController.create);
 estimateRoutes.get("/project/:projectId", checkToken, estimateController.findByProject);
