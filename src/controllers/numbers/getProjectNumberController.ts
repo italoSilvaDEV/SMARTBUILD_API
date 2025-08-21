0497f1eb366db9637208dde4c2cf86e82f831c7e
riangon <rian.goncallves@gmail.com>
2025-08-21 00:32:17 -0300

    [teste]

--- src/controllers/numbers/getProjectNumberController.ts
+++ src/controllers/numbers/getProjectNumberController.ts
@@ -23,8 +23,7 @@ export class GetEstimateNumberController {
             })
 
             return res.status(200).json({
-                number: number.length + 1000,
-                number2: number[0].number
+                number2: Number(number[0].number) + 1
             })
         } catch (error) {
             return res.status(500).json({
