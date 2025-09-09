045e52d6e17807a353c8ed0c6ac1be7344d29b13
riangon <rian.goncallves@gmail.com>
2025-09-09 13:57:01 -0300

    [fix] filtro data

--- src/controllers/TimeCards/getByWorkerIdController.ts
+++ src/controllers/TimeCards/getByWorkerIdController.ts
@@ -123,6 +123,7 @@ export class getByWorkerIdController {
                     user_id: workerId,
                     check_in_time: {
                         gte: startDate,
+                        lte: deadlineDate,
                     },
                     OR: [
                         {
