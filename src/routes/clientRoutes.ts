c84fb631e5c77551dcd4e3498b8478cdb3361a0c
yurybena <82118965+yurybena@users.noreply.github.com>
2025-11-04 18:49:08 -0300

    [feat] rotas e controller para dashboard client

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from '../controllers/client/CreateClientContro
 import { UpdateClientController } from '../controllers/client/UpdateClientController';
 import { ListClientController } from '../controllers/client/ListClientController';
 import { GetClientFinancialDetailsController } from '../controllers/client/GetClientFinancialDetailsController';
+import { ClientDashboardController } from '../controllers/client/ClientDashboardController';
 import { checkToken } from '../middlewares/checkToken';
 import { GetClientController } from '../controllers/client/getClientController';
 
@@ -12,7 +13,8 @@ const createClientController = new CreateClientController();
 const updateClientController = new UpdateClientController();
 const listClientController = new ListClientController();
 const getClientFinancialDetailsController = new GetClientFinancialDetailsController();
-const getClientController = new GetClientController(); 
+const getClientController = new GetClientController();
+const clientDashboardController = new ClientDashboardController();
 
 clientRoutes.post('/client', checkToken, createClientController.handle);//ok novo modelo
 clientRoutes.put('/client/:id', checkToken, updateClientController.handle); //ok novo modelo
@@ -20,5 +22,10 @@ clientRoutes.get('/clients', checkToken, listClientController.handleNewClients);
 clientRoutes.get('/clients-with-work-contexts', checkToken, listClientController.handleClientsWithWorkContexts); // clientes com work contexts
 clientRoutes.get('/client/financial/:email', checkToken, getClientFinancialDetailsController.handle);
 clientRoutes.get('/client/:id', checkToken, getClientController.handle);
+
+// Client Dashboard Routes
+clientRoutes.get('/client-dashboard/:clientId/charts/projects', checkToken, clientDashboardController.projectsChart);
+clientRoutes.get('/client-dashboard/:clientId/charts/estimates', checkToken, clientDashboardController.estimatesChart);
+clientRoutes.get('/client-dashboard/:clientId/charts/invoices', checkToken, clientDashboardController.invoicesChart);
 
 export { clientRoutes };
