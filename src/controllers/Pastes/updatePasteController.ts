0945a0fcebdc1345beddd7207b33edcd9539d99d
Italo <italosilva@acad.ifma.edu.br>
2025-11-07 15:46:58 -0300

    [feat] controllers para gerenciamento de pastas

---
+++ src/controllers/Pastes/updatePasteController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { prisma } from "../../utils/prisma";
+
+export class UpdatePasteController {
+    async handle(req: Request, res: Response) {
+        const {
+            id,
+            name
+        } = req.body
+
+        if (!id || !name) {
+            return res.status(400).json({
+                error: "Id and name are required"
+            })
+        }
+
+        try {
+            const paste = await prisma.projectPastes.update({
+                where: {
+                    id
+                }, 
+                data: {
+                    name
+                }
+            })
+
+            return res.status(200).json({
+                success: true,
+                message: "Paste updated successfully",
+                data: paste
+            })
+        } catch (error) {
+            return res.status(500).json({
+                error: "Internal server error"
+            })
+        }
+    }
+}
