a10e65cf14655c29d48fa35068b312adf37bafd1
riangon <rian.goncallves@gmail.com>
2025-11-07 22:59:31 -0300

    fix

--- src/controllers/Files/getFiles.ts
+++ src/controllers/Files/getFiles.ts
@@ -47,10 +47,23 @@ export class GetFilesController {
                 }
             })
 
+            const filesWithUrl = await Promise.all(files.map(async (file) => {
+                let fileUrl = null
+
+                if (file.file) {
+                    fileUrl = await getPresignedUrl(file.file)
+                }
+
+                return {
+                    ...file,
+                    file: fileUrl
+                }
+            }))
+
             return res.status(200).json({
                 success: true,
                 message: "Files fetched successfully",
-                data: files
+                data: filesWithUrl
             })
         } catch (error) {
             return res.status(500).json({
