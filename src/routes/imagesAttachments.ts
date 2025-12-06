786e265209e2c3515f200c1015d1faf8c704381e
riangon <rian.goncallves@gmail.com>
2025-12-06 00:25:51 -0300

    [fix] multer

--- src/routes/imagesAttachments.ts
+++ src/routes/imagesAttachments.ts
@@ -2,14 +2,19 @@ import { Router } from "express";
 import { UploadController } from "../controllers/imagesAttachments/uploadController";
 import { DeleteImagesAttachmentsController } from "../controllers/imagesAttachments/deleteController";
 import { checkToken } from "../middlewares/checkToken";
+import multer from "multer";
+import uploadConfig from "../config/upload";
 
 const imagesAttachmentsRoutes = Router();
 const uploadController = new UploadController();
 const deleteController = new DeleteImagesAttachmentsController();
 
+const upload = multer(uploadConfig.upload("./public/tmp/images-attachments"));
+
 imagesAttachmentsRoutes.post(
     "/images-attachments/upload",
     checkToken,
+    upload.single("file"),
     uploadController.handle.bind(uploadController)
 );
 
