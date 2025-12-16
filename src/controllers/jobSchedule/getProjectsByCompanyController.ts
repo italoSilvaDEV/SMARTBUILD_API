0aae021aa87880a4581f540426c4f91a80137ac9
riangon <rian.goncallves@gmail.com>
2025-12-16 05:30:35 -0300

    [fix] orderBy

--- src/controllers/jobSchedule/getProjectsByCompanyController.ts
+++ src/controllers/jobSchedule/getProjectsByCompanyController.ts
@@ -61,6 +61,9 @@ export class GetProjectsByCompanyController {
                             price: true,
                         }
                     }
+                },
+                orderBy: {
+                    status_project: "asc"
                 }
             })
 
