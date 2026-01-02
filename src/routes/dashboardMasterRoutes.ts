e60879f40d2eb94bc5b164e14f818d79c857a843
yurybena <82118965+yurybena@users.noreply.github.com>
2026-01-02 13:15:12 -0300

    [feat] update de dados do cliente, extra employ e dateEnd de subscription

--- src/routes/dashboardMasterRoutes.ts
+++ src/routes/dashboardMasterRoutes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { DashboardController } from "../controllers/MasterDashboard/DashboardController";
-import { FindClientById } from "../controllers/MasterDashboard/findClient";
+import { FindClientById, GetClientEditData, UpdateClientData } from "../controllers/MasterDashboard/findClient";
 import { checkToken } from "../middlewares/checkToken";
 
 const dashboardMasterRoutes = Router();
 const dashboardController = new DashboardController();
 const findClientController = new FindClientById();
+const getClientEditDataController = new GetClientEditData();
+const updateClientDataController = new UpdateClientData();
 
 dashboardMasterRoutes.get("/master-dashboard", checkToken, dashboardController.handle);
 dashboardMasterRoutes.get("/client/details/:companyId", checkToken, findClientController.handle);
+dashboardMasterRoutes.get("/client/edit-data/:companyId", checkToken, getClientEditDataController.handle);
+dashboardMasterRoutes.put("/client/update/:companyId", checkToken, updateClientDataController.handle);
 
 export { dashboardMasterRoutes };
