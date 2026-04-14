6b08c00227ea36bde6b96467d1512a86bf9a5a50
riangon <rian.goncallves@gmail.com>
2026-04-14 02:58:27 -0300

    [fix] avatar user

--- src/controllers/service/UserServiceProjectController.ts
+++ src/controllers/service/UserServiceProjectController.ts
@@ -210,14 +210,16 @@ export class UserServiceProjectController {
       });
 
       // Formatar o resultado
-      const result = employees.map((employee) => {
+      const result = await Promise.all(employees.map(async (employee) => {
         const isLinked = employee.UserServiceProject.some(
           (usp) => usp.service_project?.id === id
         );
 
         return {
           id: employee.id,
-          avatar: employee.avatar,
+          avatar: employee.avatar && employee.avatar !== "null"
+            ? await getPresignedUrl(employee.avatar)
+            : null,
           name: employee.name,
           isLinked, // Retorna true se o usuário estiver vinculado ao serviço
           office: employee.companies.find((c) => c.companyId === id_company)?.office.name,
